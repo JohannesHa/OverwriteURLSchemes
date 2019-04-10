27d6ac5792f5d92477fbbdd0dda53c8c0178bf20
Johannes <johanneshage97@gmail.com>
2019-04-10 16:21:25 +0200

    Initial Commit

---
+++ URLSchemeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLSchemeTest
+//
+//  Created by Johannes Hagemann on 10.04.19.
+//  Copyright © 2019 FirstBlink UG (haftungsbeschränkt). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
